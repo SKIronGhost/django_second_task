df21d341d05e3c7c7936062c770f97bda4ff882d
SKIronGhost <ed.diegofelix@gmail.com>
2021-01-20 23:24:15 -0300

    add task to display time with bootstrap finished

--- display_time/views.py
+++ display_time/views.py
@@ -5,7 +5,7 @@ from time import gmtime, strftime
 def display_time(request):
 
     context = {
-        "time": strftime("%H:%M %p", gmtime()),
+        "time": strftime("%H:%M %p", gmtime(-4)),
         "date": strftime("%B %d, %Y", gmtime())
     }
     return render(request, 'index.html', context)
