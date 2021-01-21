4fac5f7b05c8d2549e7ad9674e88507a98412994
SKIronGhost <ed.diegofelix@gmail.com>
2021-01-20 23:20:19 -0300

    add task to display time with bootstrap finished

---
+++ display_time/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import HttpResponse, render
+from time import gmtime, strftime
+
+
+def display_time(request):
+
+    context = {
+        "time": strftime("%H:%M %p", gmtime()),
+        "date": strftime("%B %d, %Y", gmtime())
+    }
+    return render(request, 'index.html', context)
